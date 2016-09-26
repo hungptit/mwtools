2cbc803fb19097d50df933fc52a085ec97c5224f
hungdang <hungptit@gmail.com>
2016-09-26 10:31:57 -0400

    Add parseTestLog command.

--- testing/parseTestLog.cpp
+++ testing/parseTestLog.cpp
@@ -1,11 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+
 #include "boost/program_options.hpp"
 #include "boost/lexical_cast.hpp"
 #include "boost/filesystem.hpp"
-
-#include <algorithm>
-#include <cstdlib>
-#include <iostream>
-#include <iterator>
 
 int main(int ac, char *av[]) {
     namespace po = boost::program_options;
@@ -15,8 +13,8 @@ int main(int ac, char *av[]) {
     boost::system::error_code errcode;
     path currentFolder = boost::filesystem::current_path();
     std::string logFolder;
-    std::string defaultDataFolder = (currentFolder / path("views")).string();
-    std::string defaultViewFolder = (currentFolder / path("data")).string();;
+    std::string defaultDataFolder = (currentFolder / path("data")).string();
+    std::string defaultViewFolder = (currentFolder / path("views")).string();;
     std::string defaultCommand("/local/projects/testloganalyzer/bin/logdb.pl");
 
     std::string dataFolder, viewFolder, command;
@@ -32,17 +30,15 @@ int main(int ac, char *av[]) {
         
     po::positional_options_description p;
     p.add("log-folder", -1);
-
     po::variables_map vm;
     po::store(po::command_line_parser(ac, av).options(desc).positional(p).run(), vm);
     po::notify(vm);
-
-    // Parse input arguments
     if (vm.count("help")) {
         std::cout << desc;
         return EXIT_SUCCESS;
     }
 
+    // Execute the test log parser command.
     std::string excCmd = command + " -t " + logFolder + " -d " + dataFolder + " -v " + viewFolder;
     std::cout << excCmd << "\n";
     return std::system(excCmd.c_str());
