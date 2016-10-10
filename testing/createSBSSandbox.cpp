edb35580c2b2b8aa2eca650c5a3d967333c3ff6e
hungdang <hungptit@gmail.com>
2016-10-10 19:32:54 -0400

    Update sbs related commands.

--- testing/createSBSSandbox.cpp
+++ testing/createSBSSandbox.cpp
@@ -5,56 +5,91 @@
 #include <cstdlib>
 #include <iostream>
 #include <iterator>
+#include <stdexcept>
 
 #include "tools/Resources.hpp"
 #include "utils/Process.hpp"
 #include "utils/Utils.hpp"
 
-bool parseInputParameters(int ac, char *av[]) {
-    using namespace boost;
-    namespace po = boost::program_options;
-
-    try {
-        po::options_description desc("Allowed options");
-        desc.add_options()(
-            "help,h", "This command will launch Matlab based on the given input arguments.")(
-            "cluster,c", po::value<std::string>(), "Used cluster.");
-
-        po::positional_options_description p;
-        p.add("cluster", -1);
-
-        po::variables_map vm;
-        po::store(po::command_line_parser(ac, av).options(desc).positional(p).run(), vm);
-        po::notify(vm);
-
-        // Parse input arguments
-        if (vm.count("help")) {
-            std::cout << "Usage: createSBSSandbox -c Bmdlref\n";
-            std::cout << desc;
-            return false;
+std::vector<std::string> getRootLevelFolders(const boost::filesystem::path &rootPath) {
+    namespace fs = boost::filesystem;
+    std::vector<std::string> results;
+    fs::directory_iterator endIter;
+    fs::directory_iterator dirIter(rootPath);
+    for (; dirIter != endIter; ++dirIter) {
+        auto const status = dirIter->status();
+        auto const ftype = status.type();
+        if (ftype == boost::filesystem::directory_file) {
+            results.emplace_back(dirIter->path().string());
         }
-
-        std::string cluster;
-        if (vm.count("cluster")) {
-            cluster += vm["cluster"].as<std::string>();
-        } else {
-            cluster = "Bmdlref";
-        }
-
-        const std::string cmdStr = "mw -using " + cluster + " sbs -c " + cluster;
-        std::system(cmdStr.c_str());
-        std::cout << "Command: " << cmdStr << std::endl;
     }
+    return results;
+}
 
-    catch (std::exception &e) {
-        std::cout << e.what() << "\n";
-        return false;
-    }
 
-    return true;
+void createSandbox(const std::string &cluster) {
+    const std::string cmdStr = "mw -using " + cluster + " sbs -c " + cluster;
+    std::system(cmdStr.c_str());
+    std::cout << "Command: " << cmdStr << "\n";
 }
 
 int main(int ac, char *av[]) {
-    parseInputParameters(ac, av);
+    using namespace boost;
+    namespace po = boost::program_options;
+
+    po::options_description desc("Allowed options");
+
+    std::string cluster;
+    std::vector<std::string> alias;
+    
+    // clang-format off
+    desc.add_options()
+        ("help,h", "This command will launch Matlab based on the given input arguments.")
+        ("cluster,c", po::value<std::string>(&cluster)->default_value("Bmdlref"), "Used cluster.")
+        ("alias,a", po::value<std::vector<std::string>>(&alias), "Aliasses for created sandboxes.");
+    // clang-format on
+    
+    po::positional_options_description p;
+    p.add("alias", -1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(ac, av).options(desc).positional(p).run(), vm);
+    po::notify(vm);
+
+    // Parse input arguments
+    if (vm.count("help")) {
+        std::cout << desc;
+        return EXIT_SUCCESS;
+    }
+
+    if (alias.empty()) {
+        createSandbox(cluster);
+    } else {
+        const boost::filesystem::path rootPath = boost::filesystem::current_path();
+        std::vector<std::string> originalFolders = getRootLevelFolders(rootPath);
+        std::unordered_set<std::string> dict(originalFolders.begin(), originalFolders.end());
+        std::for_each(alias.begin(), alias.end(), [&dict, &cluster, &rootPath](auto const &anAlias) {
+            createSandbox(cluster);
+        
+            // Find the new folder which is a new sandbox. This assumption is not process safed.
+            const auto currentFolders = getRootLevelFolders(rootPath);
+            for (auto aFolder : currentFolders) {
+                if (dict.find(aFolder) == dict.end()) {
+                    dict.insert(aFolder);
+                    dict.insert(anAlias);
+                
+                    // Now create a symbolic link to the new sandbox and exit the for loop.
+                    const std::string createAliasCommand = "ln -sf " + aFolder + " " + anAlias;
+                    std::system(createAliasCommand.c_str());
+                    std::cout << "Command: " << createAliasCommand << "\n";
+                    break;
+                }
+            }
+        });
+        
+    }
+
+
+
     return EXIT_SUCCESS;
 }
--- testing/deleteSBSSandbox.cpp
+++ testing/deleteSBSSandbox.cpp
@@ -7,73 +7,46 @@
 #include <iterator>
 
 #include "tools/Resources.hpp"
+#include "utils/FileUtils.hpp"
 #include "utils/Process.hpp"
 #include "utils/Utils.hpp"
 
-bool parseInputParameters(int ac, char *av[]) {
+int main(int argc, char *argv[]) {
     using namespace boost;
     namespace po = boost::program_options;
 
-    try {
-        po::options_description desc("Allowed options");
-        desc.add_options()(
-            "help,h", "This command will launch Matlab based on the given input arguments.")(
-            "cluster,c", po::value<std::string>(), "Used cluster.")(
-            "sandbox,s", po::value<std::string>(), "Sandboxes want to discard.");
+    std::vector<std::string> sandboxes;
+    std::string cluster;
 
-        po::positional_options_description p;
-        p.add("sandbox", -1);
+    po::options_description desc("Allowed options");
 
-        po::variables_map vm;
-        po::store(po::command_line_parser(ac, av).options(desc).positional(p).run(), vm);
-        po::notify(vm);
+    // clang-format off
+    desc.add_options()
+        ("help,h", "This command will delete given SBS sanboxes.")
+        ("cluster,c", po::value<std::string>(&cluster)->default_value("Bmdlref"), "Synced cluster.")
+        ("sandbox,s", po::value<std::vector<std::string>>(&sandboxes), "Sandboxes want to discard.");
+    // clang-format on
 
-        // Parse input arguments
-        if (vm.count("help")) {
-            std::cout << "Usage: createSBSSandbox -c Bmdlref\n";
-            std::cout << desc;
-            return false;
-        }
+    po::positional_options_description p;
+    p.add("sandbox", -1);
 
-        std::string cluster;
-        if (vm.count("cluster")) {
-            cluster = vm["cluster"].as<std::string>();
-        } else {
-            cluster = "Bmdlref";
-        }
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+    po::notify(vm);
 
-        std::string sandbox;
-        if (vm.count("sandbox")) {
-            sandbox = vm["sandbox"].as<std::string>();
-        } else {
-            std::cerr << "You will need to provide a sandbox name.\n";
-            return 1;
-        }
+    // Parse input arguments
+    if (vm.count("help")) {
+        std::cout << desc;
+        return EXIT_SUCCESS;
+    }
 
-        // Remove the trailing slash
-        auto it = --sandbox.end();
-        for (; it != sandbox.begin(); --it) {
-            if (*it != '/') {
-                break;
-            }
-        }
-        std::string sandboxName(sandbox.begin(), ++it);
+    std::for_each(sandboxes.begin(), sandboxes.end(), [&cluster](const auto &aSandbox) {
         std::string userName = std::getenv("USER");
-        const std::string cmdStr =
-            "mw -using " + cluster + " sbs clone discard " + userName + "." + sandboxName;
+        const std::string cmdStr = "mw -using " + cluster + " sbs clone discard " + userName +
+                                   "." + utils::normalize_path(aSandbox);
         std::system(cmdStr.c_str());
         std::cout << "Command: " << cmdStr << std::endl;
-    }
+    });
 
-    catch (std::exception &e) {
-        std::cout << e.what() << "\n";
-        return false;
-    }
-
-    return true;
-}
-
-int main(int argc, char *argv[]) {
-    parseInputParameters(argc, argv);
     return EXIT_SUCCESS;
 }
